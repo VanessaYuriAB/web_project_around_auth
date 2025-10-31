a2b65af193868985d22740b166c266ab40b98c91
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2025-10-31 11:00:48 -0300

    fix: change BrowserRouter to HashRouter to adjust page reload on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 
 import App from './components/App.jsx';
 
@@ -8,8 +8,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
